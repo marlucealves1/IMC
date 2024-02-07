b30f24bf0c061cb058d008d0122faabcb7c48e64
marlucealves1 <159350143+marlucealves1@users.noreply.github.com>
2024-02-07 17:50:50 -0300

    Add files via upload

---
+++ calculadoraIMC.js
@@ -0,0 +1,10 @@
+const soma(){
+const valor1 = documentBuid("valor1")
+const valor2 = documentBuid("valor2")
+
+const numero1 = number(valor1.value)
+const numero2 = number(valor1.value)
+const soma = numero1 + numero2
+document.getElementById("resultado").inner = "resultado" + soma
+
+}
